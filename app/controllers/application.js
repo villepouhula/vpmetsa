0fe7694fa9ee8d4e99551be25ce00528086bf4e1
Ville Pouhula <ville.pouhula@gmail.com>
2017-02-03 15:51:58 +0200

    changes

---
+++ app/controllers/application.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+   actions: {
+       transitionToSettings: function(){
+
+       },
+       transitionToProfile: function(){
+
+       },
+       toggleShare: function(){
+
+       }
+   }
+});
